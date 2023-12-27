ca028906ffd635b58b9f32437d430d2f7b9a4f21
simran9464 <146087575+simran9464@users.noreply.github.com>
2023-12-27 19:22:04 +0530

    CMS

---
+++ Cms/packages/core/admin/shared/contracts/users.ts
@@ -0,0 +1,66 @@
+import type { errors } from '@strapi/utils';
+import type { SanitizedAdminUser, Permission } from './shared';
+
+/**
+ * GET /users/me - Log in as an admin user
+ */
+export declare namespace GetMe {
+  export interface Request {
+    query: {};
+    body: {};
+  }
+
+  export interface Response {
+    data: SanitizedAdminUser;
+    error?: errors.ApplicationError;
+  }
+}
+
+/**
+ * PUT /users/me - Update the current admin user
+ */
+export declare namespace UpdateMe {
+  export interface BaseRequestBody {
+    password?: never;
+    currentPassword?: never;
+    email?: string;
+    firstname?: string;
+    lastname?: string;
+    username?: string;
+    preferedLanguage?: string;
+  }
+
+  export interface PasswordRequestBody
+    extends Omit<BaseRequestBody, 'password' | 'currentPassword'> {
+    currentPassword: string;
+    password: string;
+  }
+
+  export interface Request {
+    query: {};
+    body: BaseRequestBody | PasswordRequestBody;
+  }
+
+  export interface Response {
+    data: SanitizedAdminUser;
+    error?:
+      | errors.ApplicationError
+      | errors.ValidationError<'ValidationError', { currentPassword: ['Invalid credentials'] }>
+      | errors.YupValidationError;
+  }
+}
+
+/**
+ * GET /users/me/permissions - Get the permissions of the current admin user
+ */
+export declare namespace GetOwnPermissions {
+  export interface Request {
+    query: {};
+    body: {};
+  }
+
+  export interface Response {
+    data: Permission[];
+    error?: errors.ApplicationError;
+  }
+}
